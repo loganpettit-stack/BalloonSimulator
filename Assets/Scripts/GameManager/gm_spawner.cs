85d7f995dc46ac28c5690f620a4ef9fbd71ed1db
Not me <crud41@yahoo.com>
2019-10-15 21:13:09 -0500

    added startup code

---
+++ Assets/Scripts/GameManager/gm_spawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class gm_spawner : MonoBehaviour
+{
+    public GameObject _balloonPrefabRef;
+
+    void Awake()
+    {
+       (Instantiate(_balloonPrefabRef) as GameObject).name = "BalloonInstance";
+    }
+
+}
---
+++ Assets/Scripts/GameManager/gm_uislidehook.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class gm_uislidehook : MonoBehaviour
+{
+    public float _sizeScaleWeight = 1.0f;
+    private GameObject _balloonInstanceRef;
+
+    void Start()
+    {
+        _balloonInstanceRef = GameObject.FindGameObjectWithTag("Balloon");
+    }
+
+    public void resizeBalloon(float scale)
+    {
+        float newScale = _sizeScaleWeight * scale;
+        _balloonInstanceRef.GetComponent<Transform>().localScale = new Vector3(newScale, newScale, _balloonInstanceRef.GetComponent<Transform>().localScale.z);
+    }
+}
