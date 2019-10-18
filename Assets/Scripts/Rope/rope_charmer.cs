80c227a36b39c87b8105c1a845b0fea80d920778
Not me <crud41@yahoo.com>
2019-10-18 16:44:40 -0500

    forgot to add the file

---
+++ Assets/Scripts/Rope/rope_charmer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rope_charmer : MonoBehaviour
+{
+    void Start()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+        rb.inertiaTensor = new Vector3(1.0f, 2.0f, 3.0f);
+        rb.inertiaTensorRotation = new Quaternion(1.0f, 2.0f, 3.0f, 1.0f);
+    }
+}
