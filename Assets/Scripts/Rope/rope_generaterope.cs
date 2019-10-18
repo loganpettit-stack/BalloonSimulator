a849aee0674e69b577b0418d85fc159d8ee963c6
Not me <crud41@yahoo.com>
2019-10-17 22:02:49 -0500

    added visible string

---
+++ Assets/Scripts/Rope/rope_generaterope.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class rope_generaterope : MonoBehaviour
+{
+    public GameObject _link;
+    public int _numSegments;
+
+    void Start()
+    {
+        GenerateRope();
+    }
+
+    private void GenerateRope()
+    {
+        Rigidbody rbprev = this.GetComponent<Rigidbody>();
+
+        for (int i = 1; i <= _numSegments; i++)
+        {
+            GameObject currentLink = Instantiate(_link) as GameObject;
+            HingeJoint joint = currentLink.GetComponent<HingeJoint>();
+            joint.connectedBody = rbprev;
+
+            if (i == _numSegments)
+                GameObject.FindGameObjectWithTag("Destination").GetComponent<HingeJoint>().connectedBody = currentLink.GetComponent<Rigidbody>();
+            else
+                rbprev = currentLink.GetComponent<Rigidbody>();
+        }
+    }
+}
