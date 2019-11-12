e46739b746e75e5203267b1ac7f773321b71a3a5
Jacob Potter <japzq4@mail.umsl.edu>
2019-11-12 13:14:44 -0600

    Dome

---
+++ Assets/Scripts/DomeCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DomeCollider : MonoBehaviour
+{
+    Transform balloon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        balloon = GameObject.Find("ROOT/BALLOON").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = Vector3.Lerp(Vector3.one * 2.4f, Vector3.one * 8f, (balloon.localScale.x - 0.5f) / 3.5f);
+    }
+}
