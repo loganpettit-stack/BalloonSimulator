711e3020471eea151677fe3dfab91af3820f5e31
Nelson <nelsonbluemoon@gmail.com>
2019-11-13 16:45:07 -0600

    Added function for balloon data object

---
+++ Assets/Scripts/GameManager/dataCollect.cs
@@ -0,0 +1,40 @@
+﻿/* dataCollect.cs
+ * 11.13.2019
+ * Balloon Physics Simulator
+ * Author: Team NoName
+ * Description: Script contains a class for balloon data. It also creates a
+ * balloon object that is updated using other functions.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dataCollect : MonoBehaviour
+{
+    public gm_uiValuesUpdate uiValues;
+    // balloon object
+    public BalloonData balloonData = new BalloonData();
+
+    // Update is called once per frame
+    void Update()
+    {
+        // updates balloon object's values
+        balloonData.currentRadiusM = uiValues.currentRadiusM;
+        balloonData.surfaceArea = uiValues.surfaceArea;
+        balloonData.volume = uiValues.volume;
+        balloonData.liftForce = uiValues.liftForce;
+        //Debug.Log(" Radius: " + data.currentRadiusM + " SA: " + data.surfaceArea + " Volume: " + data.volume + " Lift Force: " + data.liftForce);
+    }
+}
+
+// class of balloon properties
+public class BalloonData {
+    public float currentRadiusM;
+    public float surfaceArea;
+    public float volume;
+    public float liftForce;
+    public float mass;
+}
+
+
