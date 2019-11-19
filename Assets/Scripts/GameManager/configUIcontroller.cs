3cbcc51fecc55359144d1222034aa29116dbb8e0
Nelson <nelsonbluemoon@gmail.com>
2019-11-18 21:01:21 -0600

    Added new contraints to controller

--- Assets/Scripts/GameManager/configUIcontroller.cs
+++ Assets/Scripts/GameManager/configUIcontroller.cs
@@ -14,45 +14,57 @@ using UnityEngine;
 public class configUIcontroller : MonoBehaviour
 {
     public JSONconfig config;
+    // booleans declared to store values from config file
     private bool colorWheel;
     private bool radiusSlider;
     private bool windSlider;
     private bool inflateDeflateButton;
+    private bool recordButton;
     private bool graph;
     private bool dataBox;
+    // referenced game objects for enabling and disabling
     public GameObject colorPanel;
     public GameObject radiusPanel;
     public GameObject windPanel;
     public GameObject radiusWindPanel;
     public GameObject sliderPanel;
     public GameObject inflateDeflatePanel;
+    public GameObject recordBtn;
+    public GameObject magnifyBtn;
+    public GameObject exportBtn;
     public GameObject graphPanel;
     public GameObject dataPanel;
     public GameObject leftPanel;
     public GameObject rightPanel;
-    //public GameObject exportBtn;
 
     // Start is called before the first frame update
     void Start()
     {
+        // setting game objects to corresponding UI components
         colorPanel = GameObject.Find("COLOR_PICKER");
         radiusPanel = GameObject.Find("PANEL_RADIUS");
         windPanel = GameObject.Find("PANEL_WIND");
         radiusWindPanel = GameObject.Find("CPANEL_TOP_R");
         sliderPanel = GameObject.Find("CPANEL_BOTTOM_R");
         inflateDeflatePanel = GameObject.Find("CPANEL_INFLATE_DEFLATE_BUTTON");
+        exportBtn = GameObject.Find("BTN_EXPORT");
+        recordBtn = GameObject.Find("BTN_RECORD");
+        magnifyBtn = GameObject.Find("BTN_MAGNIFY");
         graphPanel = GameObject.Find("CPANEL_BOTTOM_L");
         dataPanel = GameObject.Find("CPANEL_TOP_L");
         leftPanel = GameObject.Find("CPANEL_LEFT");
         rightPanel = GameObject.Find("CPANEL_RIGHT");
 
+        // setting boolean values from config file
         colorWheel = config.loadedConfig.colorWheel;
         radiusSlider = config.loadedConfig.radiusSlider;
         windSlider = config.loadedConfig.windSlider;
         inflateDeflateButton = config.loadedConfig.inflateDeflateButton;
+        recordButton = config.loadedConfig.recordButton;
         graph = config.loadedConfig.graph;
         dataBox = config.loadedConfig.dataBox;
 
+        // color wheel UI control
         if (colorWheel == true)
         {
             colorPanel.SetActive(true);
@@ -63,7 +75,7 @@ public class configUIcontroller : MonoBehaviour
         }
 
         // right panel UI control
-        if (radiusSlider == true && windSlider == true)
+        if (radiusSlider == true && windSlider == true) // manages radius slider, wind slider, and respective switches
         {
             radiusPanel.SetActive(true);
             windPanel.SetActive(false);
@@ -86,6 +98,7 @@ public class configUIcontroller : MonoBehaviour
             rightPanel.SetActive(false);
         }
 
+        // enables/disables inflate and deflate buttons
         if (inflateDeflateButton == true)
         {
             inflateDeflatePanel.SetActive(true);
@@ -96,14 +109,27 @@ public class configUIcontroller : MonoBehaviour
         }
 
         // left panel UI control
-        if (graph == true)
+
+        if (graph == true && recordButton == true) // enables graph, record, magnify ... disables export
         {
-            graphPanel.SetActive(true);
-        } else
+            recordBtn.SetActive(true);
+            magnifyBtn.SetActive(true);
+            exportBtn.SetActive(false);
+        }
+        else if (graph == false && recordButton == true) // disables graph, record, magnify ... enables export
+        {
+            recordBtn.SetActive(false);
+            magnifyBtn.SetActive(false);
+            exportBtn.SetActive(true);
+        }
+
+        // disables entire graph panel
+        if (recordButton == false)
         {
             graphPanel.SetActive(false);
         }
 
+        // enables/disables data box
         if (dataBox == true)
         {
             dataPanel.SetActive(true);
@@ -113,7 +139,8 @@ public class configUIcontroller : MonoBehaviour
             dataPanel.SetActive(false);
         }
 
-        if (graph == false && dataBox == false)
+        // enables/disables entire left panel
+        if (graph == false && dataBox == false && recordButton == false)
         {
             leftPanel.SetActive(false);
         }
