0c283cd710de26f795d6c04bddcc14dfac661dd3
Cody <ch3rc@mail.umsl.edu>
2019-11-21 07:06:40 -0600

    Added csv export script

---
+++ Assets/Scripts/GameManager/CSVexport.cs
@@ -0,0 +1,50 @@
+﻿/* CSVexport.cs
+ * 11.30.2019
+ * Balloon Physics Simulator
+ * Author: Team NoName
+ * Description: Exports radius,surface area, volume, lift force,
+ * mass, wind speed and weight to CSV file
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+using System;
+
+public class CSVexport : MonoBehaviour
+{
+
+    public dataCollect collect;
+    private string dirname = "Exports/CSV";
+    
+    private void Start()
+    {
+        /*Initial creation of CSV directory*/
+        if(!Directory.Exists(dirname))
+        {
+            Directory.CreateDirectory(dirname);
+        }
+
+        
+        Save();
+    }
+    
+    /*Record balloon info and write to csv file*/
+    public void Save()
+    {
+       if (Directory.Exists(dirname))
+       {
+            string placeHolder = DateTime.UtcNow.ToString("dd MMMM, yyyy");
+            string filePath = dirname + "/" + placeHolder + ".csv";
+            StreamWriter outStream = System.IO.File.CreateText(filePath);
+            string test = collect.getCSVFormat();
+            outStream.WriteLine(test);
+            outStream.Flush();
+            outStream.Close();
+            //Debug.Log("it worked");
+       }
+    }
+    
+        
+}
