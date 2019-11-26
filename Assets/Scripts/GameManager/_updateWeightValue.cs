a864679136975a1c3b1c58f2cb9bed95d52261f6
Cody <ch3rc@mail.umsl.edu>
2019-11-26 13:27:13 -0600

    Change text on 1000Kg weight

---
+++ Assets/Scripts/GameManager/_updateWeightValue.cs
@@ -0,0 +1,44 @@
+﻿/* _updateWeightValue.cs
+ * 11.30.2019
+ * Balloon Physics Simulator
+ * Author: Team NoName
+ * Description: Change the value of the weight based on 
+ * balloon lift force plus wind.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class _updateWeightValue : MonoBehaviour
+{
+    public Wind weight;
+    public float force;
+    public GameObject value;
+    public float val;
+
+
+    /*Grab TextMesh Pro object*/
+    void Start()
+    {
+        value = GameObject.Find("ROOT/WEIGHT/Canvas/Text").gameObject;
+        val = 1000;
+    }
+    /*Update force on weight*/
+    void Update()
+    {
+        force = Wind.WeightForce();
+
+        if(force > 0)
+        {
+            var test = val - (force/9.81f);
+            value.GetComponent<TextMeshProUGUI>().text = test.ToString("F0") + "Kg";
+        }
+        
+
+    }
+
+
+}
